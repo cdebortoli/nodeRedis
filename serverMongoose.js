d09aaca35cbe2256e1a4427906fb179dbe33bd79
cdebortoli <debortolichris@free.fr>
2014-05-01 16:02:48 +0000

    Mongoose

---
+++ serverMongoose.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/tasks');
+
+var Schema = mongoose.Schema;
+var Tasks = new Schema({
+    project: String,
+    description: String
+});
+mongoose.model('Task', Tasks);
+
+
+var Task = mongoose.model('Task');
+var task = new Task();
+task.project = 'Bikeshed';
+task.description = 'Paint the bikeshed red.';
+task.save(function(err) {
+    if (err) throw err;
+    console.log('Task saved.');
+});
+
+var Task = mongoose.model('Task');
+Task.find({'project': 'Bikeshed'}, function(err, tasks) {
+    for (var i = 0; i < tasks.length; i++) {
+        console.log('ID:' + tasks[i]._id);
+        console.log(tasks[i].description);
+    }
+});
+
+
+var Task = mongoose.model('Task');
+Task.update(
+    {_id: '53626ef37cc64768038470b5'},
+    {description: 'Paint the bikeshed green.'},
+    {multi: false},
+    function(err, rows_updated) {
+        if (err) throw err;
+        console.log('Updated.');
+    }
+);
